fd5d82bf1ee123883545a970d7708788820bf0a9
ksapozhnikov <ksapozhnikov@spbtv.com>
2017-03-01 16:54:55 +0300

    Add implementation for string anagram

---
+++ string_anagrams.rb
@@ -0,0 +1,33 @@
+#!/bin/ruby
+
+# a = gets.strip
+# b = gets.strip
+
+a = 'cde'
+b = 'abc'
+
+def calc_frequency(string, storage)
+  string.each_char do |letter|
+    storage[letter] = storage.fetch(letter, 0) + 1
+  end
+  storage
+end
+
+frequency_a = calc_frequency(a, {})
+frequency_b = calc_frequency(b, {})
+
+delete_count = 0
+
+frequency_a.keys.each do |letter|
+  count_a = frequency_a[letter]
+  count_b = frequency_b.fetch(letter, 0)
+  frequency_b.delete(letter)
+  delete_count += (count_b - count_a).abs
+end
+
+frequency_b.values.each do |value|
+  delete_count += value
+end
+
+puts delete_count
+
